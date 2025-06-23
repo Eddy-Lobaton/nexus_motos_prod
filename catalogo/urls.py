f3d1a457c144227bc81ade0ef66e52342200df20
consorciowas <eddy-pither.lobaton-caceres@watersolutions.com.pe>
2025-06-23 18:52:54 -0500

    23 06 2025

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.catalogo_productos, name='catalogo_productos'),
+    path('producto/<int:prod_id>/', views.detalle_catalogo, name='detalle_catalogo'),
+
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from tienda.models import TblProducto
 
+
 def catalogo_productos(request):
-    productos = TblProducto.objects.filter(prod_estado=True).order_by('prod_nombre')
-    return render(request, 'catalogo/catalogo.html', {'productos': productos})
+    productos = TblProducto.objects.filter(prod_estado=True)
+    return render(request, 'catalogo/catalogo_productos.html', {'productos': productos})
+
+def detalle_catalogo(request, producto_id):
+    producto = get_object_or_404(TblProducto, prod_id=producto_id, prod_estado=True)
+    return render(request, 'catalogo/detalle_catalogo.html', {'producto': producto})
