bb8e07095d4d7c03f37b9bbf2e5503ebe36a98c0
consorciowas <eddy-pither.lobaton-caceres@watersolutions.com.pe>
2025-06-23 23:55:50 -0500

    23 junio v3

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from tienda.models import TblProducto
 
 
@@ -6,6 +7,9 @@ def catalogo_productos(request):
     productos = TblProducto.objects.filter(prod_estado=True)
     return render(request, 'catalogo/catalogo_productos.html', {'productos': productos})
 
-def detalle_catalogo(request, producto_id):
-    producto = get_object_or_404(TblProducto, prod_id=producto_id, prod_estado=True)
-    return render(request, 'catalogo/detalle_catalogo.html', {'producto': producto})
+def detalle_catalogo(request, prod_id):
+    try:
+        producto = get_object_or_404(TblProducto, prod_id=prod_id, prod_estado=True)
+        return render(request, 'catalogo/detalle_catalogo.html', {'producto': producto})
+    except Exception as e:
+        return HttpResponse(f"Error al cargar producto: {e}")
